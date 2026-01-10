d87986db13154fe5a7bc152103941404faea4ab7
Nabeel1401 <fathikaan786@gmail.com>
2026-01-10 23:54:54 +0100

    updated

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -81,7 +81,7 @@ const ProtectedPage = ({ children }) => {
       <div>
         <div className="flex justify-between items-center bg-primary p-5">
           <h1 className="text-2xl text-white cursor-pointer" onClick={()=> navigate('/')}>
-          OnLine eXchange
+          Sell & Buy
           </h1>
          <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
           <FiUserCheck size={22} className="mr-2"/>
